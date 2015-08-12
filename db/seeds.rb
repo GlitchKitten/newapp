25a57cddebcaaead3921b3530d09a5aaf4e0ea83
Amber Mulholland <ahaschenburger@gmail.com>
2015-08-12 14:38:37 +0000

    product data added through seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Product.create([{name: 'Denim Shirt', description: 'Comfy shirt for walks in the woods and nights by the fire', image_url: 'denimcamerasmall.jpg'}])
+Product.create([{name: 'Denim Shirt', description: 'Comfy shirt for walks in the woods and nights by the fire', image_url: 'denimcamerasmall.jpg'}, {name: 'Suede boots', description: 'These boots are made for walking...and for looking good', image_url: 'boots.jpg'}, {name: 'Checkered Shirt', description: 'For the manliest of men only', image_url: 'pipelakesmall.jpg'}])
