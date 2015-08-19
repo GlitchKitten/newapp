a28bdd2e35c85f1f9d02c9613c6b923b061c1468
Amber Mulholland <ahaschenburger@gmail.com>
2015-08-19 14:55:54 +0000

    dynamic links added along with new root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :products
+   
+  get 'products/index'
   
   get 'static_pages/about'
 
@@ -7,13 +8,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  get 'products/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -63,5 +63,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :products
   resources :orders, only: [:index, :show, :new, :create]
 end
