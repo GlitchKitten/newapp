0bbde12bf214ec4da7a8cb21a418101163d1cf8b
Amber Mulholland <ahaschenburger@gmail.com>
2015-08-26 10:16:44 +0000

    mailer added to contact page, linked to heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,14 @@ class StaticPagesController < ApplicationController
     @featured_product = Product.limit(3)
   end
   
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'ahaschenburger@gmail.com',
+      :subject => "A new contact message from #{@name}",
+      :body => @message).deliver
+  end
+  
 end
