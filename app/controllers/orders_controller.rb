ab9351151cef277bbb66620cfeee6e80013da972
Amber Mulholland <ahaschenburger@gmail.com>
2015-09-02 17:42:30 +0000

    orders now viewable only by authorized

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_order, only: [:show, :update, :destroy]
+  load_and_authorize_resource
+
   before_filter :authenticate_user!
   
   def index
