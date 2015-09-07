61bcb0b63eb5d7f74320a2df5dfb5611c5741cec
Amber Mulholland <ahaschenburger@gmail.com>
2015-09-07 09:28:36 +0000

    stock values added to products seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Product.create([{name: 'Denim Shirt', description: 'Comfy shirt for walks in the woods and nights by the fire', image_url: 'denimcamerasmall.jpg', colour: 'Blue', price: '29.95'}, {name: 'Suede boots', description: 'These boots are made for walking...and for looking good', image_url: 'boots.jpg', colour: 'Brown', price: '49.95'}, {name: 'Checkered Shirt', description: 'For the manliest of men only', image_url: 'pipelakesmall.jpg', colour: 'Red', price: '25.95'}])
+Product.create!([{name: 'Denim Shirt', description: 'Comfy shirt for walks in the woods and nights by the fire', image_url: 'denimcamerasmall.jpg', colour: 'Blue', price: '29.95', stock: '5'}, {name: 'Suede boots', description: 'These boots are made for walking...and for looking good', image_url: 'boots.jpg', colour: 'Brown', price: '49.95', stock: '1'}, {name: 'Checkered Shirt', description: 'For the manliest of men only', image_url: 'pipelakesmall.jpg', colour: 'Red', price: '25.95', stock: '7'}])
