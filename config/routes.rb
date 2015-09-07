194b0c4341b05e102d3d9f6f4e07a7850eba5e01
Amber Mulholland <ahaschenburger@gmail.com>
2015-09-07 12:02:40 +0000

    comment controller and new comment partial created, strong parameters set

--- config/routes.rb
+++ config/routes.rb
@@ -71,6 +71,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :products
+  resources :products do
+    resources :comments
+  end
+  
   resources :orders, only: [:index, :show, :new, :create]
 end
