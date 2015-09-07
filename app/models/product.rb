f03ed433e42256df597b0447bac9c8204ca902ca
Amber Mulholland <ahaschenburger@gmail.com>
2015-09-07 08:54:04 +0000

    validation or numericality added for price and stock fields

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :orders
   has_many :comments
+  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0}
+  validates :stock, :numericality => {:greater_than_or_equal => 0}
 end
