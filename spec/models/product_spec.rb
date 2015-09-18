994d970159fd7679829433c75d4cfcd4f2575373
Amber Mulholland <ahaschenburger@gmail.com>
2015-09-18 12:38:56 +0000

    validations for missing user password and email added

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
   describe Product do
     describe "#average_rating" do
-      context "one or more ratings present"
+      context "one or more ratings present" do
         before do
           @product = Product.create!(name: "shirt", price: "1", stock: "1")
           @user = User.create!(first_name: "Test", email: "Test@gmail.com", password: "12345678")
@@ -12,10 +12,11 @@ require 'rails_helper'
         
         it 'returns the average rating of all comments' do
           expect(@product.average_rating).to eq 3
-        end      
+        end
+      end  
     end
     
-    context "product created without name"
+    context "product created without name" do
       before do
         @product = Product.create(price: "1", stock: "1", description: "shirt")
       end
@@ -23,6 +24,7 @@ require 'rails_helper'
       it 'product creation is not valid' do
         expect(@product).not_to be_valid
       end
+    end  
   end  
             
         
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,37 @@ require 'rails_helper'
 
   describe User do
   
-    context "first name present" do
-      before :each do 
+    context "all user information present" do
+      before do 
         @user = User.create(first_name: "Mary", last_name: "Smith", email: "marysmith@gmail.com", password: "12345678")
       end
-      it "should return first name" do
+      
+      it "should return all information" do
         expect(@user.first_name).to eq "Mary"
+        expect(@user.last_name).to eq "Smith"
+        expect(@user.email).to eq "marysmith@gmail.com"
+        expect(@user.password).to eq "12345678"
       end
-    end
+    end  
+    
+    context "user email is not provided" do
+      before do
+        @user = User.create(first_name: "Mary", last_name: "Smith", password: "12345678")
+      end
+   
+      it "should not save the user" do
+        expect(@user).not_to be_valid
+      end
+    end  
+    
+    context "user password is not provided" do
+      before do
+        @user = User.create(first_name: "Mary", last_name: "Smith", email: "marysmith@gmail.com")
+      end
+   
+      it "should not save the user" do
+        expect(@user).not_to be_valid
+      end
+    end  
   end
+ 
