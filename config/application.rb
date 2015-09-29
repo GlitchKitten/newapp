bbb0c3c56374f377efcd27ed563acb6dbd4f3db1
Amber Mulholland <ahaschenburger@gmail.com>
2015-09-29 09:17:42 +0000

    more keys

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,5 @@ module Newapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
   end
 end
