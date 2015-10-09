e98d8d0e5041aa0fbe578d5c3f160a5cf0c334d2
Amber Mulholland <ahaschenburger@gmail.com>
2015-10-09 12:59:38 +0000

    passwords now filtered in log

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ Bundler.require(*Rails.groups)
 
 module Newapp
   class Application < Rails::Application
+    config.filter_parameters<<:password
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
