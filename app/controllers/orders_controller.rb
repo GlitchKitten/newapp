8cdb53d79de18adc754291acdaff8088bf41d133
Amber Mulholland <ahaschenburger@gmail.com>
2015-10-12 13:30:02 +0000

    order view tweaks

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :update, :destroy]
+  before_filter :authenticate_user!
   load_and_authorize_resource
   
   def index
